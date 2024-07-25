d5966041bef74bb69debcbec7ab6014e51d9eb8c
CounselJS <okpabicounsel@gmail.com>
2024-07-25 18:06:35 +0100

    fixing redux

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,37 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import ModalSlice from "./slices/modalSlice";
+
 import AuthSlice from "./slices/auth/authSlice";
-import Analytics from "./slices/analytics/analyticsSlice";
-import UserManagmtSlice from "./slices/userManagmt/userManagmtSlice";
-import CategorySlice from "./slices/category/categorySlice";
-import ProductSlice from "./slices/products/productSlice";
-import CartSlice from "./slices/cart/cartSlice";
-import OrderSlice from "./slices/orders/orderSlice";
-import TransactionSlice from "./slices/transaction/transactionSlice";
-import WishlistSlice from "./slices/wishlist/wishlistSlice";
-import CheckoutSlice from "./slices/checkout/checkoutSlice";
-import FaqSlice from "./slices/faq/faqSlice";
-import CustomerSlice from "./slices/customers/customerSlice";
-import StaffSlice from "./slices/staff/staffSlice";
-import TicketSlice from "./slices/tickets/ticketSlice";
 
 export const store = configureStore({
   reducer: {
-    modal: ModalSlice,
     auth: AuthSlice,
-    userMgmt: UserManagmtSlice,
-    analytics: Analytics,
-    categories: CategorySlice,
-    cart: CartSlice,
-    products: ProductSlice,
-    orders: OrderSlice,
-    transactions: TransactionSlice,
-    wishlist: WishlistSlice,
-    checkout: CheckoutSlice,
-    faq: FaqSlice,
-    customer: CustomerSlice,
-    staff: StaffSlice,
-    ticket: TicketSlice,
   },
 });
 
